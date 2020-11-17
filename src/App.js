5f7be19ed63ecf4f44aa1a400a12fdc593d97323
Lior Vaknin <liorvak303@gmail.com>
2020-11-17 09:26:42 +0200

    chapter 6 proj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component'
+import { Route, Switch } from 'react-router-dom';
+
+const NewPage = () => (
+  <div>
+    <h1>NEW PAGE!</h1>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={NewPage} />
+      </Switch>
     </div>
   );
 }
