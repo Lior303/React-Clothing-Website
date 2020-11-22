4df6d6e4f7c4f1fca253306ef6488c8c055dd8b2
Lior Vaknin <liorvak303@gmail.com>
2020-11-22 11:15:23 +0200

    added the cart dropdown functionallity and adding items into the cart. added grouping to the items so we dont have any repeats

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,29 +1,36 @@
 import React from 'react'
 import './collection-item.styles.scss'
 import CustomButton from '../custom-button/custom-button.component';
+import { connect } from "react-redux";
+import {addItem} from "../../redux/cart/cart.actions";
 
 const CollectionItem = (
-    {name, id, price, imageUrl}
-) => (
-    <div className='collection-item'>
+    {item, addItem}
+) => {
+    return (
+        <div className='collection-item'>
 
-        <div
-        className='image'
-        style={{
-            backgroundImage: `url(${imageUrl})`
-        }}
-        />
+            <div
+                className='image'
+                style={{
+                    backgroundImage: `url(${item.imageUrl})`
+                }}
+            />
 
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
+            <div className='collection-footer'>
+                <span className='name'>{item.name}</span>
+                <span className='price'>{item.price}</span>
+            </div>
+            <CustomButton inverted onClick={() => addItem(item)}>
+                Add to cart
+            </CustomButton>
         </div>
-        <CustomButton inverted>
-            Add to cart
-        </CustomButton>
-    </div>
-);
-
+    )
+};
 //if it is not equal to anything it is true --- for the inverted property
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.slice(0, 4).map(({id, ...otherCollectionItem}) => (
-                    <CollectionItem key={id} {...otherCollectionItem}/>
+                items.slice(0, 4).map((item) => (
+                    <CollectionItem key={item.id} item={item}/>
                 ))
             }
         </div>
