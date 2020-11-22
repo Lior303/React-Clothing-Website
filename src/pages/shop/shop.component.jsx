62726603d8adbd9bb65edddc03c912c686118913
Lior Vaknin <liorvak303@gmail.com>
2020-11-22 18:24:15 +0200

    moved the data to its own reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,10 @@
 import React from 'react';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
-import SHOP_DATA from './shop.data'
-import CollectionPreview from '../../components/preview-collection/preview-collection.component'
-
-class ShopPage extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        return (<div className='shop-page'>
-            {
-                this.state.collections
-                    .map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))
-            }
-        </div>)
-    }
-}
+const ShopPage = () => (
+    <div className='shop-page'>
+        <CollectionOverview/>
+    </div>
+);
 
 export default ShopPage;
